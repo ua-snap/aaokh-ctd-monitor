f353227afa2f3e13fb214eef77465f77ba189d5f
Craig Stephenson <crstephenson@alaska.edu>
2017-08-29 10:27:03 -0800

    Initial commit. Scan and display images from the "plots" directory.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>AAOKH CTD Data</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"></link>
+  </head>
+  <body>
+    <div class="header">
+      <div class="container">
+        <img src="images/aaokh_header.png" />
+      </div>
+    </div>
+    <div class="container">
+    <?php
+      $directory = 'plots';
+      foreach (scandir($directory) as $file) {
+        if (!in_array($file, array('.', '..'))) {
+          $fullPath = $directory . '/' . $file;
+          print '<h3>' . date('F d Y', filemtime($fullPath)) . '</h3>';
+          print '<a href="' . $fullPath . '"><img src="' . $fullPath . '" class="plot"></a>'; 
+        }
+      }
+    ?>
+    </div>
+  </body>
+</html>
